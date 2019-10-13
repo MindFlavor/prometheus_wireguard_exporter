608ed03f17ac7ea7304be4a50dd4d469956112d6
Francesco Cogno <francesco.cogno@outlook.com>
2019-10-13 17:45:19 +0200

    added unit tests

--- src/options.rs
+++ src/options.rs
@@ -38,3 +38,32 @@ impl Options {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_interface_some() {
+        let options = Options {
+            verbose: true,
+            separate_allowed_ips: false,
+            extract_names_config_file: Some("/etc/wireguard/wg0.conf".to_owned()),
+            export_remote_ip_and_port: true,
+        };
+
+        assert_eq!(options.get_interface(), Some("wg0"));
+    }
+
+    #[test]
+    fn test_interface_none() {
+        let options = Options {
+            verbose: true,
+            separate_allowed_ips: false,
+            extract_names_config_file: None,
+            export_remote_ip_and_port: true,
+        };
+
+        assert_eq!(options.get_interface(), None);
+    }
+}
